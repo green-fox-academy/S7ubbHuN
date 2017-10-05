240894ea1c55abd89be821b9128e9a1390c54546
S7ubbHuN <sassbalazs@gmail.com>
2017-10-05 15:58:29 +0200

    finished diamond in last project.

--- week-01/day-4/day4 (13-17)/main.c
+++ week-01/day-4/day4 (13-17)/main.c
@@ -15,20 +15,23 @@ create a program which draws a diamond, like this:
     * * *
       *
 */
-int c, k;
-
+int c, r;
 
     for ( c = 1 ; c <= 7 ; c++ )
     {
-        for( k = 1 ; k <= c ; k++ )
-            printf("*");
+        for( r = 7 ; r >= c ; r-- )
+            printf(" ");
+        for( r = 1 ; r <= c ; r++ )
+            printf("* ");
 
         printf("\n");
     }
     for ( c = 7 ; c >= 1 ; c-- )
     {
-        for( k = 1 ; k <= c ; k++ )
-            printf("*");
+        for( r = 7 ; r >= c ; r-- )
+            printf(" ");
+        for( r = 1 ; r <= c ; r++ )
+            printf("* ");
 
         printf("\n");
     }
