07e05c13d8f3d8fb7832370a728bd0e7cc847afc
S7ubbHuN <sassbalazs@gmail.com>
2017-10-05 15:26:20 +0200

    Starting diamond in last task

--- week-01/day-4/day4 (13-17)/main.c
+++ week-01/day-4/day4 (13-17)/main.c
@@ -3,6 +3,36 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    //17.feladat
+    /*
+create a program which draws a diamond, like this:
+      *
+    * * *
+  * * * * *
+* * * * * * *
+* * * * * * *
+  * * * * *
+    * * *
+      *
+*/
+int c, k;
+
+
+    for ( c = 1 ; c <= 7 ; c++ )
+    {
+        for( k = 1 ; k <= c ; k++ )
+            printf("*");
+
+        printf("\n");
+    }
+    for ( c = 7 ; c >= 1 ; c-- )
+    {
+        for( k = 1 ; k <= c ; k++ )
+            printf("*");
+
+        printf("\n");
+    }
+
+
+  return 0;
 }
