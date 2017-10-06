9d3818c5540114e82366a7fd0179bb5a8f494bf6
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 14:50:14 +0200

    Modified digit counter, task6

--- week-01/day-5/day5_task6(armstrong)/main.c
+++ week-01/day-5/day5_task6(armstrong)/main.c
@@ -3,14 +3,21 @@
 
 int main()
 {
-    int num=0, digit=0;
+    int strnum=0, num=0, digit=0;
     printf("Enter a number: \n");
-    scanf ("%d", &num);
+    scanf ("%d", &strnum);
     printf("--------------------------\n");
+
+    num=strnum;
     while (num!=0){
         num/=10;
-        digit++
+        digit++;
     }
+
+
+
+
+    printf("%d", digit);
 
     return 0;
 }
