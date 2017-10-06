f10ebc4374f0f8aaeedbce2676a222721a8aa2b7
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 15:37:40 +0200

    Made style formatting

--- week-01/day-5/day5_task6(armstrong)/main.c
+++ week-01/day-5/day5_task6(armstrong)/main.c
@@ -7,27 +7,20 @@ int main()
     printf("Enter a number: \n");
     scanf ("%d", &onum);
     printf("--------------------------\n");
-
     num=onum;
     while (num!=0) {
         num /= 10;
         digit++;
     }
-
     num=onum;
     while (num != 0) {
         rem = num%10;
         powsum += pow(rem, digit);
         num /= 10;
     }
-    if (powsum == onum){
+    if (powsum == onum)
         printf("This number is an Armstrong number!");
-    }
-    else {
+    else
         printf("This number is NOT an Armstrong number!");
-    }
-
-
-
     return 0;
 }
