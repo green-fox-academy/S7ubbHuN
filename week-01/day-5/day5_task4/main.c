d65a83e95aa9db0fde356acaa653a6ad60845552
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 11:56:13 +0200

    Finished task 4

--- week-01/day-5/day5_task4/main.c
+++ week-01/day-5/day5_task4/main.c
@@ -13,7 +13,15 @@ int main()
 7777777
 if you change the rows variable the output should be different as well
 */
-
-
+    int row=0;
+    printf("Enter the number of rows: \n");
+    scanf ("%d", &row);
+    printf("--------------------------\n");
+    for (int a=1; a<=row; a++) {
+        for (int b=1; b<=a; b++) {
+                printf("%d", a);
+        }
+        printf("\n");
+    }
     return 0;
 }
