19a8ecb3e91b67c9786288b8651e39b942766d8c
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 10:56:23 +0200

    Finished task 2

--- week-01/day-5/day5_task2/main.c
+++ week-01/day-5/day5_task2/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char asc=0;
+    int n=33;
+    for(asc = '!'; asc <= '~'; asc++)
+        {
+            printf("The '%c' ASCII value in decimal is: %d\n", asc, n);
+            n=n+1;
+        }
+
     return 0;
 }
