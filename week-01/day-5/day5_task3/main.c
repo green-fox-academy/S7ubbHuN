f2504c29f81310ceaa05775c63db399554232409
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 11:08:26 +0200

    task3 v1

--- week-01/day-5/day5_task3/main.c
+++ week-01/day-5/day5_task3/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//how much odd numbers are between 179 & 371
+//you should be able to change the limits, like from 10 to 234
+//and the program should print out the odd numbers between those two numbers
+//example from 11 to 27 the program should print out:
+// 13, 15, 17, 19, 21, 23, 25 this is 7 odd number between 11 and 27
+
 int main()
 {
-    printf("Hello world!\n");
+    int a, b;
+    printf("Enter a starting number: \n");
+    scanf ("%d", &a);
+    printf("Enter an ending number: \n");
+    scanf ("%d", &b);
+    for (a=a, a<=b, a++)
+    {
+        if (a%2!=0)
+        {
+        printf("%d\n",a);
+        }
+    }
     return 0;
 }
