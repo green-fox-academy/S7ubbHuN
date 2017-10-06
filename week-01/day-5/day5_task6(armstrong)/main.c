82f9c2f5d653fc03f9ea4432eefc921c0d180049
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 14:37:02 +0200

    Adds digit counter to task6

--- week-01/day-5/day5_task6(armstrong)/main.c
+++ week-01/day-5/day5_task6(armstrong)/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+    int num=0, digit=0;
+    printf("Enter a number: \n");
+    scanf ("%d", &num);
+    printf("--------------------------\n");
+    while (num!=0){
+        num/=10;
+        digit++
+    }
 
     return 0;
 }
