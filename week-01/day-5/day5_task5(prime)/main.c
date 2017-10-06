22779e7de9ddb2cb62b59d3aef7625ded88072fe
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 13:53:10 +0200

    Testing task5

--- week-01/day-5/day5_task5(prime)/main.c
+++ week-01/day-5/day5_task5(prime)/main.c
@@ -3,6 +3,33 @@
 
 int main()
 {
+/*
+Create a program which prints out oll the prime numbers
+aslo add a feature where you can tell the from - to limits
+for example :
+from = 11;
+to = 34;
+output :
+13, 17, 19, 23, 29, 31 , this is 6 prime numbers
+*/
 
+    int strt, end, cnt=0, a=0, a2=0, b=0;
+    printf("Enter a starting number: \n");
+    scanf ("%d", &strt);
+    printf("Enter an ending number: \n");
+    scanf ("%d", &end);
+    printf("-------------------------\nThe prime number(s) are: ");
+    for (a=strt; a=end; a++) {
+        for (a2=1; a2<=a; a2++) {
+            if (a%a2==0){
+                cnt++;
+            }
+        }
+        if (cnt==2) {
+            printf("%d, ",a2);
+            b++;
+            }
+    }
+    printf("\nThis is %d prime number(s), between %d and %d.", b, strt, end);
     return 0;
 }
