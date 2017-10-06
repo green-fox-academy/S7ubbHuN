19634597dc00f42dae8b5d8e488513ab6acefde0
S7ubbHuN <sassbalazs@gmail.com>
2017-10-06 16:08:02 +0200

    Style formatting in task5

--- week-01/day-5/day5_task5(prime)/main.c
+++ week-01/day-5/day5_task5(prime)/main.c
@@ -4,8 +4,8 @@
 int main()
 {
 /*
-Create a program which prints out oll the prime numbers
-aslo add a feature where you can tell the from - to limits
+Create a program which prints out all the prime numbers
+also add a feature where you can tell the from - to limits
 for example :
 from = 11;
 to = 34;
@@ -13,26 +13,25 @@ output :
 13, 17, 19, 23, 29, 31 , this is 6 prime numbers
 */
 
-    int strt, end, cnt=0, a=0, a2=0, b=0;
+    int strt = 0, end = 0, cnt = 0, a = 0, a2 = 0, b = 0;
     printf("Enter a starting number: \n");
     scanf ("%d", &strt);
     printf("Enter an ending number: \n");
     scanf ("%d", &end);
     printf("-------------------------\nThe prime number(s) are: ");
-    for (a=strt; a<=end; a++) {
-        for (a2=1; a2<=a; a2++) {
-            if (a%a2==0){
+    for (a = strt; a <= end; a++) {
+        for (a2 = 2; a2 <= a; a2++) {
+            if (a % a2 == 0) {
                 cnt++;
-                if (cnt>2){
+                if (cnt > 2)
                     break;
-                }
             }
         }
-        if (cnt==2) {
-            printf("%d, ",a);
+        if (cnt == 2) {
+            printf("%d, ", a);
             b++;
         }
-        cnt=0;
+        cnt = 0;
     }
     printf("\nThis is %d prime number(s), between %d and %d.", b, strt, end);
     return 0;
