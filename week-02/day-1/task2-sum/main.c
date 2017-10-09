b7ecf355040af6e769900840a3ca5bd7cb788670
S7ubbHuN <sassbalazs@gmail.com>
2017-10-09 14:49:38 +0200

    Modified task2 from recursion to 'for' cycle

--- week-02/day-1/task2-sum/main.c
+++ week-02/day-1/task2-sum/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "numbers.h"
+//#define numbers 20
 
 
 // Write a function called `sum` that sum all the numbers
@@ -10,15 +11,25 @@ int sum();
 
 int main()
 {
+    scanf("%d", &numbers);
     printf("Sum: %d", sum(numbers));
     return 0;
 }
 
-int sum(int a)
+/*int sum(int a)
 {
     if(a != 0)
         return a + sum(a - 1);
     else
         return a;
 
+}
+*/
+int sum(numbers)
+{
+    int j = 0, i = 0;
+    for(i = 1; i <= numbers; i++) {
+        j += i;
+    }
+    return j;
 }
