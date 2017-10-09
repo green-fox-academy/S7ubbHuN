c313f39f2c5c3b968bfffdef9cbc15766fc0e209
S7ubbHuN <sassbalazs@gmail.com>
2017-10-09 15:26:28 +0200

    finished task3

--- week-02/day-1/task3-printer/main.c
+++ week-02/day-1/task3-printer/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "variables.h"
 
 // Create a function called `printer`
 // which prints the input parameters
 // can have multiple number of arguments
 // store your variables in a .h file
 
+int printer();
 int main()
 {
-
+    printer();
     return 0;
+}
+
+int printer(variables)
+{
+    printf("%d, ", a);
+    printf("%d, ", b);
+    printf("%d", c);
 }
