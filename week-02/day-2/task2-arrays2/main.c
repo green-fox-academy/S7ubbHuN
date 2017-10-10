980ed0c48ab3b4c90f3e7ac8df5a6f54963c7036
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 10:51:42 +0200

    finished task2

--- week-02/day-2/task2-arrays2/main.c
+++ week-02/day-2/task2-arrays2/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char char_array[] = {'a', 'b', 'c', 'd'};
+    char char_array_the_same[] = "abcd";
+
+	/* TODO:
+    Print out the two arrays character-by-character */
+    int i = 0, j = 0;
+    for (i = 0; i < sizeof char_array; i++) {
+        printf("%c", char_array[i]);
+    }
+    printf("\n");
+    for (j = 0; j < sizeof char_array_the_same; j++) {
+        printf("%c", char_array_the_same[j]);
+    }
+	return 0;
 }
