2295004cc865608c9de83dff8a0981b780f755af
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 14:47:55 +0200

    fills the array with elements

--- week-02/day-2/task8-advanced1/main.c
+++ week-02/day-2/task8-advanced1/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    printf("Hello world!\n");
+    // TODO: write a program wich asks for a number, that will be the real size of the array (50 elements are allocated, that's the maximum size)
+    // Then load up the array with integer values
+	// print out the array
+    // Then ask for a number, that will be the position, where you want the element to be deleted
+    // handle the problem when trying to delete from invalid positions (e.g. negative number)
+    // print out the the array after the deleting
+    int arr[50];
+    int ras, i, pos;
+
+    printf("Enter the length of the array (the max. is 50): ");
+    scanf("%d", &ras);
+    int real_arr[ras];
+
+    for(i = 1; i <= ras; i++) {
+        if (ras > 50) {
+            printf("This is bigger then 50!");
+            break;
+            }
+        real_arr[i] = i;
+        printf("%d ", real_arr[i]);
+    }
+    printf("Enter a position: ");
+    scanf("%d", &pos);
+
     return 0;
 }
