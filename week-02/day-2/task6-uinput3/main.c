bf63437457c73db6ed97783bdd50914dcf9c0882
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 13:23:06 +0200

    finished task6 with getch.

--- week-02/day-2/task6-uinput3/main.c
+++ week-02/day-2/task6-uinput3/main.c
@@ -5,9 +5,16 @@ int main()
 {
     //as you know, a character is represented as a numeric value(ASCII value) in the memory
     //write a function which asks for one charcter and prints out the character in character format then in ASCII too
-    printf("Type in one character then press enter: ");
 
+    printf("Type in one character: ");
     char_to_ascii();
 
     return 0;
+}
+
+void char_to_ascii()
+{
+    char chr = getch();
+    printf("\n");
+    printf("The %c character in ASCII is %d.", chr, chr);
 }
