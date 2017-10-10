7ccad3a561e5b7d1041e0ceb1bef6c54a098b2f3
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 11:20:41 +0200

    started task5

---
+++ week-02/day-2/task5-uinput2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char buffer[255];   // Buffer variable for user input
+
+    //TODO:
+    // Get the user's name with gets
+    printf("Enter your name: ");
+    gets(buffer);
+
+    //TODO:
+    // Print it out with puts
+    printf("Your name is: ");
+    puts(buffer);
+
+    //TODO:
+    // Get the user's pet name with getch
+
+    //TODO:
+    // Print it out
+
+    return 0;
+}
