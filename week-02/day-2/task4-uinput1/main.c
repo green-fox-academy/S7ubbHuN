3f3a90edae2caa5ecd3a0d5957f1fb4352a1e6a5
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 11:15:10 +0200

    finished task4

--- week-02/day-2/task4-uinput1/main.c
+++ week-02/day-2/task4-uinput1/main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char buffer[255];   // Buffer variable for user input
+
+    //TODO:
+    // Get the user's name with scanf
+    printf("Enter your name: ");
+    scanf("%s", buffer);
+
+    //TODO:
+    // Print it out with printf
+    printf("\n");
+    printf("Your name is: %s.", buffer);
+
     return 0;
 }
