893c4390dd956f5a0322e5e727a91ea1f696379a
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 15:08:38 +0200

    style formats task 8

--- week-02/day-2/task8-advanced1/main.c
+++ week-02/day-2/task8-advanced1/main.c
@@ -11,30 +11,36 @@ int main()
     // handle the problem when trying to delete from invalid positions (e.g. negative number)
     // print out the the array after the deleting
     int arr[50];
-    int ras, i, j, pos;
+    int ras, i, pos;
 
     printf("Enter the length of the array (the max. is 50): ");
     scanf("%d", &ras);
-    int real_arr[ras];
-
     for(i = 1; i <= ras; i++) {
         if (ras > 50) {
             printf("This is bigger then 50!");
             break;
         }
-        real_arr[i] = i;
-        printf("%d ", real_arr[i]);
+        else if (ras < 1) {
+            printf("This is not a positive value");
+            break;
+        }
+        arr[i] = i;
+        printf("%d, ", arr[i]);
     }
     printf("\nEnter a position: ");
     scanf("%d", &pos);
 
     for(i = 1; i <= ras; i++) {
-        if (i == pos) {
-            printf(" ");
-            i++;
+        if (pos < 1 | pos > ras) {
+            printf("This is an invalid position");
+            break;
         }
-        real_arr[i] = i;
-        printf("%d ", real_arr[i]);
+        else if (i == pos) {
+                printf("");
+                i++;
+        }
+        arr[i] = i;
+        printf("%d, ", arr[i]);
     }
     return 0;
 }
