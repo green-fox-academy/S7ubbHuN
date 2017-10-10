60068b4831d30f63f2a58e1ca6894f659ea5efbf
S7ubbHuN <sassbalazs@gmail.com>
2017-10-10 12:54:43 +0200

    started task6

---
+++ week-02/day-2/task6-uinput3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //as you know, a character is represented as a numeric value(ASCII value) in the memory
+    //write a function which asks for one charcter and prints out the character in character format then in ASCII too
+    printf("Type in one character then press enter: ");
+
+    char_to_ascii();
+
+    return 0;
+}
