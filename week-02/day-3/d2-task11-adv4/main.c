38c768c8fc95194b4c568141e354bea7f3ed89c1
S7ubbHuN <sassbalazs@gmail.com>
2017-10-11 16:19:30 +0200

    finished adv4, needs fine tuning

--- week-02/day-3/d2-task11-adv4/main.c
+++ week-02/day-3/d2-task11-adv4/main.c
@@ -9,15 +9,13 @@ int main()
     //print out the array
 
     int arr[50];
-    int i = 0, j;
-    while (i < 50) {
-        for (j = 200; j >= INT_MIN; j--) {
-            if (j % 85 == 0) {
-                arr[i] = j;
-                printf("%d, ", arr[i]);
-                i++;
-                break;
-            }
+    int i = 0;
+    int j = 0;
+    for (i = 0, j = 200; i < 50; j--) {
+        if (j % 85 == 0) {
+            arr[i] = j;
+            printf("%d, ", arr[i]);
+            i++;
         }
     }
     return 0;
