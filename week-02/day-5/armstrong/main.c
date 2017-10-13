8da0d8f0a0d3779f02e48be1fd1a5ed2207fb73d
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 11:30:24 +0200

    style formatting

--- week-02/day-5/armstrong/main.c
+++ week-02/day-5/armstrong/main.c
@@ -3,24 +3,27 @@
 
 int main()
 {
-    int onum=0, num=0, digit=0, rem=0, powsum=0;
+    int original_num=0, num=0, digit=0, remainder=0, powered_sum=0;
     printf("Enter a number: \n");
-    scanf ("%d", &onum);
+    scanf ("%d", &original_num);
     printf("--------------------------\n");
-    num=onum;
+    num=original_num;
     while (num!=0) {
         num /= 10;
         digit++;
     }
-    num=onum;
+    num=original_num;
     while (num != 0) {
-        rem = num%10;
-        powsum += pow(rem, digit);
+        remainder = num%10;
+        powered_sum += pow(remainder, digit);
         num /= 10;
     }
-    if (powsum == onum)
-        printf("This number is an Armstrong number!");
-    else
-        printf("This number is NOT an Armstrong number!");
+    if (powered_sum == original_num) {
+        printf("This number is an Armstrong number, because the %d powered sum of the digits is: %d\n", digit, powered_sum);
+        printf("It is equal to the original number: %d", original_num);
+    } else {
+        printf("This number is NOT an Armstrong number, because the %d powered sum of the digits is: %d\n", digit, powered_sum);
+        printf("It is NOT equal to the original number: %d", original_num);
+    }
     return 0;
 }
