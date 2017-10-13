e25d55f548838797b71142632233ea9cf3528201
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 10:28:56 +0200

    tunes recursive method (not working)

--- week-02/day-5/josephus_problem/main.c
+++ week-02/day-5/josephus_problem/main.c
@@ -3,22 +3,22 @@
 
 int main()
 {
-    int soldiers_number, pos;
+    int soldiers_number, starting_pos;
     printf("Enter the number of soldiers: ");
     scanf("%d", &soldiers_number);
-    if (soldiers_soldiers_number < 1) {
+    if (soldiers_number < 1) {
             printf("This is not a positive number");
             exit(0);
     }
-    pos = 2;
+    starting_pos = 2;
 
-    printf("The chosen place is %d to survive", circle(soldiers_number, pos));
+    printf("The chosen place is %d to survive", circle(soldiers_number, starting_pos));
     return 0;
 }
 
-int circle(soldiers_number, pos)
+int circle(soldiers_number, starting_pos)
 {
-
+    return (soldiers_number, starting_pos);
 
 }
 
