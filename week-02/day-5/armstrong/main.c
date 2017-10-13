b52d546da80d58ceb99bf9c2b2983e7e23815818
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 11:18:24 +0200

    added user input to armstrong

---
+++ week-02/day-5/armstrong/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int onum=0, num=0, digit=0, rem=0, powsum=0;
+    printf("Enter a number: \n");
+    scanf ("%d", &onum);
+    printf("--------------------------\n");
+    num=onum;
+    while (num!=0) {
+        num /= 10;
+        digit++;
+    }
+    num=onum;
+    while (num != 0) {
+        rem = num%10;
+        powsum += pow(rem, digit);
+        num /= 10;
+    }
+    if (powsum == onum)
+        printf("This number is an Armstrong number!");
+    else
+        printf("This number is NOT an Armstrong number!");
+    return 0;
+}
