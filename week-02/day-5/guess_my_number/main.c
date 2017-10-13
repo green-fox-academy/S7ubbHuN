02683241daf67f14865c5eecc0e0e70d83d764a3
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 13:42:12 +0200

    adds range and random between range (guessing)

--- week-02/day-5/guess_my_number/main.c
+++ week-02/day-5/guess_my_number/main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int first_element, last_element;
+    int array[255];
+
+    printf("Enter the first element:");
+    scanf("%d", &first_element);
+    printf("Enter the last element:");
+    scanf("%d", &last_element);
+    int i = 0;
+    int j = (last_element - first_element);
+    while (i <= j) {
+        array[i] = (first_element + i);
+        //printf("%d. element is %d. \n",i + 1,array[i]);
+        i++;
+    }
+    //srand(time(NULL));
+    int number = (rand() % (last_element - first_element)) + first_element;
+    printf("%d", number);
+
+
+
+
     return 0;
 }
