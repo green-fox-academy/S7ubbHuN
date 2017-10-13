7228479a13d39a82848a56b1404d5045588aeb44
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 14:03:36 +0200

    style formatting and simplify number guessing

--- week-02/day-5/guess_my_number/main.c
+++ week-02/day-5/guess_my_number/main.c
@@ -12,29 +12,27 @@ You can add lives. (optional)
 int main()
 {
     int first_element, last_element, guess, lives = 5;
-    int array[255];
+    //int array[255];
 
     printf("Enter the first element:");
     scanf("%d", &first_element);
     printf("Enter the last element:");
     scanf("%d", &last_element);
+    int array[last_element - first_element];
     int i = 0;
     int j = (last_element - first_element);
     while (i <= j) {
         array[i] = (first_element + i);
-        //printf("%d. element is %d. \n",i + 1,array[i]);
         i++;
     }
     srand(time(NULL));
     int number = (rand() % (last_element - first_element)) + first_element;
-    //printf("%d", number);
-
     while (lives > 0) {
     printf("You have %d lives left. Enter your guess:", lives);
     scanf("%d", &guess);
         if (guess == number) {
             printf("\nYou won! My number was: %d", number);
-            break;
+            exit(0);
         } else if (guess > number) {
                 printf("\nWrong! Your guess is bigger than my number!\n");
                 lives--;
@@ -43,5 +41,6 @@ int main()
                 lives--;
         }
     }
+    printf("\nYOUR ARE DEAD! My number was %d. Thanks for playing!\n", number);
     return 0;
 }
