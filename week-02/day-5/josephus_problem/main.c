88129999ab85d4817a13b32cb809b068eebc0887
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 10:12:44 +0200

    adds counting mechanism

--- week-02/day-5/josephus_problem/main.c
+++ week-02/day-5/josephus_problem/main.c
@@ -3,6 +3,37 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int arr[10];
+    int soldiers_number, i, pos;
+
+    printf("Enter the number of soldiers (between 1 and 255): ");
+    scanf("%d", &soldiers_number);
+    if (soldiers_number > 255 | soldiers_number < 1) {
+            printf("This is not a valid number");
+            exit(0);
+    }
+
+    /*for(i = 1; i <= soldiers_number; i++) {
+        arr[i] = i;
+    }*/
+    i = 0;
+    pos = i + 1;
+
+    while (i <= soldiers_number) {
+        arr[i] = i;
+        for(i = 1; i <= soldiers_number; i++) {
+            if (i == pos) {
+                soldiers_number = soldiers_number - 1;
+                for(i = pos; i <= soldiers_number; i++) {
+                    arr[i] = i + 1;
+                    printf("%d, ", arr[i]);
+                }
+            }
+            i++;
+        }
+    }
+    return arr[1];
 }
+
+
+
