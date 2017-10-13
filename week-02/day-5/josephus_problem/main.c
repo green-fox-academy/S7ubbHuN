b4380468a6d61a588a482eda5e2e2da9bbdfed05
S7ubbHuN <sassbalazs@gmail.com>
2017-10-13 10:22:08 +0200

    adds counting mechanism with recursive method

--- week-02/day-5/josephus_problem/main.c
+++ week-02/day-5/josephus_problem/main.c
@@ -3,37 +3,23 @@
 
 int main()
 {
-    int arr[10];
-    int soldiers_number, i, pos;
-
-    printf("Enter the number of soldiers (between 1 and 255): ");
+    int soldiers_number, pos;
+    printf("Enter the number of soldiers: ");
     scanf("%d", &soldiers_number);
-    if (soldiers_number > 255 | soldiers_number < 1) {
-            printf("This is not a valid number");
+    if (soldiers_soldiers_number < 1) {
+            printf("This is not a positive number");
             exit(0);
     }
+    pos = 2;
 
-    /*for(i = 1; i <= soldiers_number; i++) {
-        arr[i] = i;
-    }*/
-    i = 0;
-    pos = i + 1;
-
-    while (i <= soldiers_number) {
-        arr[i] = i;
-        for(i = 1; i <= soldiers_number; i++) {
-            if (i == pos) {
-                soldiers_number = soldiers_number - 1;
-                for(i = pos; i <= soldiers_number; i++) {
-                    arr[i] = i + 1;
-                    printf("%d, ", arr[i]);
-                }
-            }
-            i++;
-        }
-    }
-    return arr[1];
+    printf("The chosen place is %d to survive", circle(soldiers_number, pos));
+    return 0;
 }
 
+int circle(soldiers_number, pos)
+{
+
+
+}
 
 
