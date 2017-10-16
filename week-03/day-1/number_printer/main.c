ef3806b030e3e718a1e6e8a170886fe677b64503
S7ubbHuN <sassbalazs@gmail.com>
2017-10-16 15:22:57 +0200

    repaired number printer

--- week-03/day-1/number_printer/main.c
+++ week-03/day-1/number_printer/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printf_numbers(int from, int to)
+void printf_numbers(unsigned int from, unsigned int to)
 {
-	for (int i = from; i < to; i++)
-		printf("%d,", i);
+    for (unsigned int i = from; i < to; i++)
+		printf("%u,", i);
 }
 
 int main()
 {
-	printf_numbers(2147483600, 2147484600);
+	printf_numbers(2147483600,2147484600); //"to" is out of int boundary, have to use ulong
 	return 0;
 }
