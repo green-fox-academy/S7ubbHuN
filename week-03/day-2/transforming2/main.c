1ad0d8338f5ee718e5bd87e103561bc4f9e7acab
S7ubbHuN <sassbalazs@gmail.com>
2017-10-17 11:41:13 +0200

    finished transforming2

---
+++ week-03/day-2/transforming2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	float val;
+	char str[5];
+	strcpy(str, "3.14");
+
+	//TODO: print out the value of pi, first as a string, then a float value.
+
+	val = atof(str);
+    printf("%s, %f", str, val);
+
+	return(0);
+}
