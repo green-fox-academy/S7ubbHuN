3da8ff65a22262f22ec35d7d097fc81f8f86c016
S7ubbHuN <sassbalazs@gmail.com>
2017-10-17 17:43:17 +0200

    adds working dec, bin, oct to other functions

--- week-03/calc/conversion/main.c
+++ week-03/calc/conversion/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//dec to other, bin to dec, okt to dec, hex to dec, greet+numbers asking
+//needs: dec to other, bin to dec+other, okt to dec+other, hex to dec+other, greet+numbers asking(?)
 
-void dec_to_other()
+
+void dec_to_other(char* str, int new_base)
 {
     char buffer[20];
-    number = atoi(str);
+    int number = atoi(str);
+    //printf("%s", str);
     if (new_base == 2) {
         itoa(number, buffer, 2);
         printf("%s", buffer);
@@ -19,27 +21,47 @@ void dec_to_other()
     }
 }
 
-int bin_to_dec(long number)
+void bin_to_other(char* str, int new_base)
 {
-    int decnum = 0, i = 0, remainder;
+    char buffer[20];
+    int number = atoi(str);
+    int decnum = 0;
+    int i = 0, remainder;
     while (number!=0) {
         remainder = number%10;
         number /= 10;
         decnum += remainder*pow(2,i);
         i++;
     }
-    return decnum;
+    if (new_base == 10) {
+        printf("%d", decnum);
+    } else if (new_base == 8) {
+        itoa(decnum, buffer, 8);
+        printf("%s", buffer);
+    } else if (new_base == 16) {
+        itoa(decnum, buffer, 16);
+        printf("%s", buffer);
+    }
 }
 
-void oct_to_dec(double number)
+void oct_to_other(char* str, int new_base)
 {
-    char oct[32]={0};
-    int  decnum = 0,i = 0, power_index = 0;
-    for(i = (strlen(oct)-1);i >= 0; i--) {
-        decnum += (oct[i]-0x30)*pow((double)8,(double)power_index);
+    //char oct[32]={0};
+    char buffer[20];
+    int  decnum = 0, i = 0, power_index = 0;
+    for(i = (strlen(str)-1);i >= 0; i--) {
+        decnum += (str[i]-0x30)*pow((double)8,(double)power_index);
         power_index++;
     }
-    return decnum;
+    if (new_base == 2) {
+        itoa(decnum, buffer, 2);
+        printf("%s", buffer);
+    } else if (new_base == 10) {
+        printf("%d", decnum);
+    } else if (new_base == 16) {
+        itoa(decnum, buffer, 16);
+        printf("%s", buffer);
+    }
 }
 
 void hex_to_dec()
@@ -47,22 +69,25 @@ void hex_to_dec()
 
 }
 
-void start(char str[], int original_base, int new_base)
-{
-    char str[20];
-    int original_base;
-    int new_base;
-    printf("Give me a number to transform it in an other system: ");
-    scanf("%s", str);
-    printf("Give me the original base: ");
-    scanf("%d", &original_base);
-    printf("Give me the number of the new base: ");
-    scanf("%d", &new_base);
-}
+
 
 
 int main()
 {
+    char str[20];
+    int original_base;
+    int new_base;
+    int decnum;
+    printf("Give me a number to transform it in an other system: ");
+    scanf("%s", &str);
+    printf("Give me the original base: ");
+    scanf("%d", &original_base);
+    printf("Give me the number of the new base: ");
+    scanf("%d", &new_base);
+
+    oct_to_other(str, new_base);
+    //printf("afterbintodecinmain: %d\n", decnum);
+
 
     return 0;
 }
