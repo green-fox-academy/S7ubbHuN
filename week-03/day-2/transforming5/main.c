f02e309ea1266b6b1ca88ff69e455420504caa2a
S7ubbHuN <sassbalazs@gmail.com>
2017-10-17 12:41:29 +0200

    finished transforming5

--- week-03/day-2/transforming5/main.c
+++ week-03/day-2/transforming5/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int i_array[10] = {260, 259, 258, 257, 256, 255, 254, 253, 252, 251};
+    unsigned char uc_array[10];
+
+	//TODO: write a program, which copies the values from the given array to an unsigned char array
+    //print out the elements of the unsigned char array, with the "%d" format specifier
+    //what do you see?
+
+    //can copy values as characters
+
+    memcpy(uc_array, i_array, strlen(i_array)+1);
+    int i = 0;
+    while (i < 10) {
+        printf("%d\n", i_array[i]);
+        i++;
+    }
+
+
     return 0;
 }
