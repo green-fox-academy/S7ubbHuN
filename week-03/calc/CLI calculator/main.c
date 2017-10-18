27fd136c68c8e9b9f13200a5cf7ede573e97903d
S7ubbHuN <sassbalazs@gmail.com>
2017-10-18 17:48:49 +0200

    modified menu

--- week-03/calc/CLI calculator/main.c
+++ week-03/calc/CLI calculator/main.c
@@ -13,44 +13,41 @@ int main()
     int number2;
 
     calc_menu();
-    scanf("%s %s %d", number1, command, &number2);
-    long numberbuffer = strtol(number1, NULL, 10);
 
-    if (strcmp(command, "+") == 0) {
-        printf("%d", sum(numberbuffer, number2));
-    } else if (strcmp(command, "-") == 0) {
-        printf("%d", subt(numberbuffer, number2));
-    } else if (strcmp(command, "*") == 0) {
-        printf("%d", multiply(numberbuffer, number2));
-    } else if (strcmp(command, "/") == 0) {
-        printf("%d", divi(numberbuffer, number2));
-    } else if (strcmp(command, "%") == 0) {
-        printf("%d", divi_w_remainder(numberbuffer, number2));
-    } else if (strcmp(command, "sqr") == 0) {
-        printf("%d", sqr(numberbuffer));
-    } else if (strcmp(command, "sqr_root") == 0) {
-        printf("%f", sqr_root(numberbuffer));
-    } else if (strcmp(command, "log") == 0) {
-        printf("%f", logx(number2, numberbuffer));
-    } else if (strcmp(command, "binto") == 0) {
-        printf("%s", binto(number1, number2));
-    } else if (strcmp(command, "octto") == 0) {
-        printf("%s", octto(number1, number2));
-    } else if (strcmp(command, "decto") == 0) {
-        printf("%s", decto(number1, number2));
-    } else if (strcmp(command, "hexto") == 0) {
-        printf("%s", hexto(number1, number2));
-    } else if (strcmp(number1, "exit") == 0) {
-        printf("The program is exiting now. Goodbye!");
-    } else if (strcmp(number1, "clear") == 0) {
-        system("cls");
-    } else if (strcmp(number1, "help") == 0) {
-        calc_menu();
+    do {
+        scanf("%s %s %d", number1, command, &number2);
+        long numberbuffer = strtol(number1, NULL, 10);
+
+        if (strcmp(command, "+") == 0) {
+            printf("%d", sum(numberbuffer, number2));
+        } else if (strcmp(command, "-") == 0) {
+            printf("%d", subt(numberbuffer, number2));
+        } else if (strcmp(command, "*") == 0) {
+            printf("%d", multiply(numberbuffer, number2));
+        } else if (strcmp(command, "/") == 0) {
+            printf("%d", divi(numberbuffer, number2));
+        } else if (strcmp(command, "%") == 0) {
+            printf("%d", divi_w_remainder(numberbuffer, number2));
+        } else if (strcmp(command, "sqr") == 0) {
+            printf("%d", sqr(numberbuffer));
+        } else if (strcmp(command, "sqr_root") == 0) {
+            printf("%f", sqr_root(numberbuffer));
+        } else if (strcmp(command, "log") == 0) {
+            printf("%f", logx(number2, numberbuffer));
+        } else if (strcmp(command, "binto") == 0) {
+            printf("%s", binto(number1, number2));
+        } else if (strcmp(command, "octto") == 0) {
+            printf("%s", octto(number1, number2));
+        } else if (strcmp(command, "decto") == 0) {
+            printf("%s", decto(number1, number2));
+        } else if (strcmp(command, "hexto") == 0) {
+            printf("%s", hexto(number1, number2));
+        } else if (strcmp(number1, "clear") == 0) {
+            system("cls");
+        } else if (strcmp(number1, "help") == 0) {
+            calc_menu();
+        }
     }
-
-
-
-
-
-
+    while (strcmp(number1, "exit") != 0);
+    printf("The program is exiting now. Goodbye!");
 }
