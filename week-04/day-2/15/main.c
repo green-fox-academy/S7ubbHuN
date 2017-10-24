b9dfedcc852c5fecb322454a39c519856fc604cb
S7ubbHuN <sassbalazs@gmail.com>
2017-10-24 16:10:58 +0200

    finished 15

--- week-04/day-2/15/main.c
+++ week-04/day-2/15/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void reverse_name(char *name, char *reversed_name);
+
+int main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+	char name[] = "Gipsz Jakab";
+
+    char reversed[50];
+
+    //write a function which reverse a name's last name and first name. Use a void function. Put your 'return' value in the second parameter.
+
+    reverse_name(name, reversed);
+    printf("%s", reversed);
+}
+
+void reverse_name(char *name, char *reversed_name)
+{
+    int i = 0;
+    int center = 0;
+    int j = 0;
+
+    while (name[i] != ' '){
+        i++;
+        center = i;
+    }
+
+    for (i = center + 1; name[i] != '\0'; i++){
+        reversed_name[j++] = name[i];
+    }
+    reversed_name[j++] = ' ';
+
+    for (i = 0; i != center; i++){
+        reversed_name[j++] = name[i];
+    }
+    reversed_name[j++] = '\0';
 }
