ce5119fd29886e4cb397162d8e41e94da518a120
S7ubbHuN <sassbalazs@gmail.com>
2017-10-24 13:19:52 +0200

    finished 01

--- week-04/day-2/01/main.c
+++ week-04/day-2/01/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int number = 1234;
     //TODO:
     // print the memory address of the variable
-
+    //int *p_number = &number;
+    printf("%x", &number);
     return 0;
 }
