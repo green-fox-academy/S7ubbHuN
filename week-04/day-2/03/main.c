aad5585c1c62ec94090889a203f5caa7ee450100
S7ubbHuN <sassbalazs@gmail.com>
2017-10-24 13:25:07 +0200

    finished 03

--- week-04/day-2/03/main.c
+++ week-04/day-2/03/main.c
@@ -5,7 +5,9 @@ int main()
     int number = 1234;
     int* number_pointer = &number;
     //TODO:
-    // print the value of number using the "number_pointer"
+    // update the value of number variable to 42 using the "number_pointer"
+    *number_pointer = 42;
     printf("%d", *number_pointer);
+
     return 0;
 }
