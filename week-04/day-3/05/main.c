de8bce8637f2f4ab2c10af80343f4d9328cf82c2
S7ubbHuN <sassbalazs@gmail.com>
2017-10-25 17:37:05 +0200

    some advance at 05

--- week-04/day-3/05/main.c
+++ week-04/day-3/05/main.c
@@ -7,17 +7,36 @@
 //  - The price in EUR
 //  - The number of rooms
 //  - The area of the house in square meters
+struct Houses {
+    char address[100];
+    float price;
+    int rooms;
+    float area;
+};
+
+//strcpy(house1.address, "address");
 
 // TODO:
 // The market price of the houses is 400 EUR / square meters
 // Create a function that takes a pointer to a house and decides if it's worth to buy
 // (if the price is lower than the calculated price from it's area)
 
+float worth_to_buy (struct Houses, house)
+{
+    if ((price/area)<400) {
+        printf("It's worth to buy!");
+    }
+
+}
+
 // TODO:
 // Create a function that takes an array of houses (and it's length), and counts the
 // houses that are worth to buy.
 
-int main() {
+int main()
+{
 
-  return 0;
+    struct Houses house1 = {"Gergely utca 118", 12000, 4, 35};
+
+    return 0;
 }
