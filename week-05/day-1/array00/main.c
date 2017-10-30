6710515defc1773fbe2ca0411f41034ac2ebc6a5
S7ubbHuN <sassbalazs@gmail.com>
2017-10-30 10:31:39 +0100

    finished array00

--- week-05/day-1/array00/main.c
+++ week-05/day-1/array00/main.c
@@ -7,16 +7,24 @@
  * Reverse the order of the numbers _in_the_array_ an print them out again.
 */
 
-
+//int i;
 
 int main()
 {
     srand(time(NULL));
     int intarray[5];
+    int i;
 
-    for (int i = 0; i < 5; i++) {
+
+    for (i = 0; i < 5; i++) {
         intarray[i] = rand();
         printf("%d | ", intarray[i]);
     }
+    printf("\n");
+    for (i = 5; i > 0; i--) {
+
+        printf("%d | ", intarray[i - 1]);
+    }
     return 0;
 }
+
