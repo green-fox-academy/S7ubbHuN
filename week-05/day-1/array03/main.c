ee3271908816242d3d13ca2045b9671afa4bdfb6
S7ubbHuN <sassbalazs@gmail.com>
2017-10-30 11:11:14 +0100

    started 03 task

---
+++ week-05/day-1/array03/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create a 2 dimensonal array of characters. 8x8
+ * Fill the array with X and space alternating.
+ * Print it out row by row. (It should resemble a chess board)
+ * Designate a character for each chess peace. (Queen could be Q, King could be K, etc.)
+ * Set up the start of a game.
+ * Print it out again.
+ * Make one move with a horse.
+ * Print it again.
+ */
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
