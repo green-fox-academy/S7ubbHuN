a79543a06036ee9830806aec8dc1bd8dff41fb84
S7ubbHuN <sassbalazs@gmail.com>
2017-10-31 10:11:13 +0100

    started array function 00

---
+++ week-05/day-2/arrayfunc00/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create a function that takes an array of integers as an argument (a pointer) and it's length as an unsigned integer.
+ * The function should sort the elements of the array in ascending order.
+ *
+ * Print it out before and after to be able to check whether it works as supposed.
+ */
+
+int sort_ascend(int *numbers[], unsigned int arr_length)
+{
+
+
+
+
+
+}
+
+
+int main()
+{
+
+    int numbers[10];
+    for (int i = 0; i < 10; i++) {
+        numbers[i] = i + 1;
+    }
+
+
+    return 0;
+}
