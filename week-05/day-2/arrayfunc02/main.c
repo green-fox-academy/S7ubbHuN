71075c2d96b19caf71bc8651106ce8f05019b131
S7ubbHuN <sassbalazs@gmail.com>
2017-10-31 15:24:41 +0100

    simplified and changed the function, works good

--- week-05/day-2/arrayfunc02/main.c
+++ week-05/day-2/arrayfunc02/main.c
@@ -16,13 +16,15 @@
 
 int char_change (char *arr1, char *arr2, unsigned int index, int arr1_l, int arr2_l)
 {
-    if (index > arr2_l) {
+    char i = 'a';
+    char j = 'A';
+    if (index > arr1_l) {
         return -1;
-    } else if ((arr2_l - index) > (arr1_l - index)) {
+    } else if (arr2_l > (arr1_l - index)) {
         return 1;
     } else {
-        for (int i = index; i < arr1_l; i++) {
-        arr1[i] = arr2[i];
+        for (i = ('a' + index); i < 'a' + index + arr2_l; i++, j++) {
+        arr1[i] = arr2[j];
         }
     }
     return 0;
@@ -32,42 +34,38 @@ int char_change (char *arr1, char *arr2, unsigned int index, int arr1_l, int arr
 int main()
 {
     char arr1[10];
-    char arr2[10];
+    char arr2[5];
     unsigned int index = 0;
     int arr1_l = (sizeof arr1 / sizeof arr1[0]);
     int arr2_l = (sizeof arr2 / sizeof arr2[0]);
 
-    //1. index is out of bounds (return = 0)
-
+    printf("arr1: ");
     for (char i = 'a'; i < ('a' + arr1_l); i++) {
         arr1[i] = i;
+        printf("%c, ", arr1[i]);
         }
+    printf("\narr2: ");
     for (char j = 'A'; j < ('A' + arr2_l); j++) {
         arr2[j] = j;
+        printf("%c, ", arr2[j]);
         }
-    index = 5;
-    printf("%d\n", char_change(arr1, arr2, index, arr1_l, arr2_l));
+
+    //1. Everythin is ok (return = 0)
+
+    index = 4;
+    printf("\n%d : ", char_change(arr1, arr2, index, arr1_l, arr2_l));
+    for (char i = 'a'; i < ('a' + arr1_l); i++) {
+        printf("%c, ", arr1[i]);
+        }
+    printf("\n");
 
     //2. index is out of bounds (return = -1)
 
-    for (char i = 'a'; i < ('a' + arr1_l); i++) {
-        arr1[i] = i;
-        }
-    for (char j = 'A'; j < ('A' + arr2_l); j++) {
-        arr2[j] = j;
-        }
     index = 15;
     printf("%d\n", char_change(arr1, arr2, index, arr1_l, arr2_l));
 
     //3. inserted content is longer than the receiving array (return 1)
 
-    arr2_l = 20;
-    for (char i = 'a'; i < ('a' + arr1_l); i++) {
-        arr1[i] = i;
-        }
-    for (char j = 'A'; j < ('A' + arr2_l); j++) {
-        arr2[j] = j;
-        }
     index = 9;
     printf("%d\n", char_change(arr1, arr2, index, arr1_l, arr2_l));
 
--- week-05/day-2/arrayfunc03/main.c
+++ week-05/day-2/arrayfunc03/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Create a function that takes an array of array of characters and returns an array of integers each representing the
+ * lenght of the given character array in the first array.
+ * Say your input is this: {"Good", "Morning", "Vietnam"}
+ * Your output should be this: {4, 7, 7}.
+ * HINT: strings are terminated with a special character: '\0'.
+ * So, "Good" as an array of characters would look like this: {'G', 'o', 'o', 'd', '\0'}.
+ *
+ * In the main function create code that demonstrates that Your function works approrpiately.
+ */
+
 int main()
 {
     printf("Hello world!\n");
