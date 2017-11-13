2e4e4da12ed2d2c4686d3450d3693ef510129323
S7ubbHuN <sassbalazs@gmail.com>
2017-11-13 15:34:40 +0100

    adds vector01 task to the code

--- week-06/day-1/vector00/main.cpp
+++ week-06/day-1/vector00/main.cpp
@@ -9,6 +9,15 @@ int main()
 {
     std::vector <int> numbers(5);
     iota( numbers.begin(), numbers.end(), 1 );
-    std::cout << numbers.at(2);
+    int i = 2;
+    std::cout << "The "<< i + 1 << "th element is " << numbers.at(i) << "!" << std::endl;
+
+    //create an integer vector with the size of 5 and print all elements of it
+
+    std::cout << "The elements are: ";
+    for(unsigned int i = 0; i< numbers.size(); ++i) {
+        std::cout << numbers[i] << ", ";
+    }
+
     return 0;
 }
