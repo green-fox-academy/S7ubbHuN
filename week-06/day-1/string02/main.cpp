e9ee55d95d05208a143b3ee95d4b33b136704076
S7ubbHuN <sassbalazs@gmail.com>
2017-11-13 13:11:25 +0100

    started string02

---
+++ week-06/day-1/string02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
+
+int main()
+{
+
+    return 0;
+}
