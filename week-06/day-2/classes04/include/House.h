198c30b2ee0a39c6d4b959c92f387805eacfea09
S7ubbHuN <sassbalazs@gmail.com>
2017-11-14 12:51:29 +0100

    started task04

---
+++ week-06/day-2/classes04/include/House.h
@@ -0,0 +1,16 @@
+#ifndef HOUSE_H
+#define HOUSE_H
+
+
+class House
+{
+    public:
+        House();
+        virtual ~House();
+
+    protected:
+
+    private:
+};
+
+#endif // HOUSE_H
---
+++ week-06/day-2/classes04/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "House.h"
+
+
+
+int main() {
+  // The market price of the houses is 400 EUR / square meters
+  // Make the get_price() function work
+  // Create a constructor for the House class that takes it's address and area.
+  House house = House("Andrassy 66", 349);
+  cout << house.get_price();
+
+  return 0;
+}
