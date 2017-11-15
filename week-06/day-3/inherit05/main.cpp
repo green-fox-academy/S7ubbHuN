47ca2aa1046436e0f73b8790def5f6608a6cb9d3
S7ubbHuN <sassbalazs@gmail.com>
2017-11-15 17:30:36 +0100

    finished task05

--- week-06/day-3/inherit05/main.cpp
+++ week-06/day-3/inherit05/main.cpp
@@ -10,6 +10,16 @@
 // the value of number of seats in the car it should tell you that 'The car is full!' or something like that.
 // Inherite at least two car models and try out the methods you have created.
 
+/*
+    if (m750i.persons_in_car <= m750i.number_of_seats && Transit.persons_in_car <= Transit.number_of_seats) {
+            m750i.add_person_to_car();
+            Transit.add_person_to_car();
+    } else if (m750i.persons_in_car == m750i.number_of_seats && Transit.persons_in_car <= Transit.number_of_seats){
+            Transit.add_person_to_car();
+    } else {
+            break;
+    }
+*/
 class Car {
     public:
         int increase_km (unsigned int runned) {
@@ -40,6 +50,9 @@ class Car {
         void set_manufacturer(std::string manufacturer) {
             this->manufacturer = manufacturer;
         }
+        void set_number_of_seats(unsigned int number_of_seats) {
+            this->number_of_seats = number_of_seats;
+        }
 
     private:
         unsigned int age = 0;
@@ -63,8 +76,25 @@ int main()
 
     m750i.set_manufacturer("Bmw 750i");
     Transit.set_manufacturer("Ford Transit");
-
+    m750i.set_number_of_seats(5);
+    Transit.set_number_of_seats(9);
     std::cout << "1. standard values\nManufacturer: "<< m750i.get_manufacturer() << " Km: " << m750i.get_km() << ", number of seats: " << m750i.get_number_of_seats() << ", persons in car: " << m750i.get_persons_in_car() << std::endl;
     std::cout << "1. standard values\nManufacturer: "<< Transit.get_manufacturer() << " Km: " << Transit.get_km() << ", number of seats: " << Transit.get_number_of_seats() << ", persons in car: " << Transit.get_persons_in_car() << std::endl;
+
+    m750i.add_person_to_car();
+    Transit.add_person_to_car();
+    std::cout << "2. +1 person\nManufacturer: "<< m750i.get_manufacturer() << " Km: " << m750i.get_km() << ", number of seats: " << m750i.get_number_of_seats() << ", persons in car: " << m750i.get_persons_in_car() << std::endl;
+    std::cout << "2. +1 person\nManufacturer: "<< Transit.get_manufacturer() << " Km: " << Transit.get_km() << ", number of seats: " << Transit.get_number_of_seats() << ", persons in car: " << Transit.get_persons_in_car() << std::endl;
+
+    std::cout << "\n\nAdding another 10 person with cycle" << std::endl;
+
+    for (int i = 0; i < 10; ++i) {
+        m750i.add_person_to_car();
+        Transit.add_person_to_car();
+    }
+
+    std::cout << "3. Manufacturer: "<< m750i.get_manufacturer() << " Km: " << m750i.get_km() << ", number of seats: " << m750i.get_number_of_seats() << ", persons in car: " << m750i.get_persons_in_car() << std::endl;
+    std::cout << "3. Manufacturer: "<< Transit.get_manufacturer() << " Km: " << Transit.get_km() << ", number of seats: " << Transit.get_number_of_seats() << ", persons in car: " << Transit.get_persons_in_car() << std::endl;
+
     return 0;
 }
