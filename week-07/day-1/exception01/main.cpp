61e1f3c3301debe0c93786c8731de803fb012121
S7ubbHuN <sassbalazs@gmail.com>
2017-11-20 13:54:19 +0100

    adds solution to task03

--- week-07/day-1/exception01/main.cpp
+++ week-07/day-1/exception01/main.cpp
@@ -22,7 +22,7 @@ int main()
     catch(int x) {
         std::cout << "you can not divide by zero! Err no.: " << x;
     }
-    return 0;*/
+    return 0;
 
     // Throw an char in the try block
 
@@ -40,6 +40,26 @@ int main()
     }
     catch(char x) {
         std::cout << "you can not divide by zero! Err no.: " << x;
+    }
+    return 0;*/
+
+
+    // Throws a bulit-in exception in the try block
+
+    try {
+        int a = 0;
+        int b = 0;
+        std::cin >> a;
+        std::cin >> b;
+
+        if (b == 0){
+            throw std::runtime_error ("you can not divide by zero!");
+        }
+        int c = a/b;
+        std::cout << c;
+    }
+    catch(std::runtime_error &err) {
+        std::cout << err.what();
     }
     return 0;
 }
