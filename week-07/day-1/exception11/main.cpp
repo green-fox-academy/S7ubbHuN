56ba6806584f7e7920c180ca5b7f1fe8bb08b605
S7ubbHuN <sassbalazs@gmail.com>
2017-11-20 15:18:56 +0100

    starteed task11

---
+++ week-07/day-1/exception11/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	srand(time(NULL));
+	int rnd = 0;
+	for (int i = 0; i < 20; ++i) {
+		rnd = rand() % 5;
+		try {
+			switch (rnd) {
+				case 0:
+					throw 3.2f;
+					break;
+				case 1:
+					throw double(43.56);
+					break;
+				case 3:
+					throw "Abrakadabra";
+					break;
+				case 4:
+					throw false;
+					break;
+				default:
+					throw 12;
+					break;
+			}
+		}
+// Write catch blocks for each possibly thrown exception.
+// In each catch block write out what the exception's type was. And what the exceptions value was.
+
+	}
+
+	return 0;
+}
