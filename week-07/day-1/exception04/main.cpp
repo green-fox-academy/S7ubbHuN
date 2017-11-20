f19c416926ddabb064055570ca0acfd0e405a386
S7ubbHuN <sassbalazs@gmail.com>
2017-11-20 14:34:30 +0100

    finsihed task04-06

--- week-07/day-1/exception04/main.cpp
+++ week-07/day-1/exception04/main.cpp
@@ -6,22 +6,42 @@
 // In the main(), put that function in a try block
 // Also in the main() catch, what your function throws
 
-void throw_int {
-
-
+void throw_int (int b) {
+    if (b == 0){
+        throw 10;
+    }
 }
 
+void throw_char (int b) {
+    if (b == 0){
+        throw 'x';
+    }
+}
 
-void throw_char
-
-
-void throw_runtime
+void throw_runtime (int b) {
+    if (b == 0){
+        throw std::runtime_error ("you can not divide by zero!");
+    }
+}
 
 
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    try {
+        int a = 0;
+        int b = 0;
+        std::cin >> a;
+        std::cin >> b;
+        throw_int(b);
+        throw_char(b);
+        throw_runtime(b);
+        int c = a/b;
+        std::cout << c;
+    }
+    catch(...) {
+        std::cout << "blablabla";
+    }
     return 0;
 }
