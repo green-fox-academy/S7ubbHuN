44ea95732e0c4c07ecacb2d70326654b005c7ae8
S7ubbHuN <sassbalazs@gmail.com>
2017-11-20 13:40:01 +0100

    finished task01

--- week-07/day-1/exception01/main.cpp
+++ week-07/day-1/exception01/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <stdexcept>
 
-using namespace std;
+// Write a try - catch block.
+// Throw an integer in the try block
+// Catch it in the catch block and write it out.
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    try {
+        int a = 0;
+        int b = 0;
+        std::cin >> a;
+        std::cin >> b;
+
+        if (b == 0){
+            throw std::runtime_error("you can not divide by zero!");
+        }
+        int c = a/b;
+        std::cout << c;
+    }
+    catch(std::runtime_error &err){
+        std::cout << err.what();
+    }
     return 0;
 }
