600a5219d830b4e470fa049d3810fd8429fc416c
S7ubbHuN <sassbalazs@gmail.com>
2017-11-20 14:00:34 +0100

    started task04-06 alltogether

---
+++ week-07/day-1/exception04/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdexcept>
+
+
+// Create a function which throws an integer
+// In the main(), put that function in a try block
+// Also in the main() catch, what your function throws
+
+void throw_int {
+
+
+}
+
+
+void throw_char
+
+
+void throw_runtime
+
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
