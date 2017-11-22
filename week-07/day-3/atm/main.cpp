45bcaba1abd9be668aab60376804c092f7220527
S7ubbHuN <sassbalazs@gmail.com>
2017-11-22 15:35:05 +0100

    adds first version of objects

--- week-07/day-3/atm/main.cpp
+++ week-07/day-3/atm/main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
 
+
+class Customer {
+    public:
+        Customer (std::string username, std::string password, int balance) {
+        this->username = username;
+        this->password = password;
+        this->balance = balance;
+        }
+        int GetBalance() {
+            return balance;
+        }
+        void DepositMoney(int depositAmount);
+        void WithdrawMoney(int withdrawalAmount);
+private:
+    std::string username;
+    std::string password;
+    int balance;
+
+};
+
+class ATM {
+    public:
+        ATM(){};
+
+    private:
+        std::vector<Customer> customers;
+        int atm_balance;
+};
+
+void Customer::DepositMoney(int depositAmount) {
+    balance = balance + depositAmount;
+}
+
+void Customer::WithdrawMoney(int withdrawalAmount) {
+    balance = balance - withdrawalAmount;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ATM atm;
+	//atm.menu();
+
     return 0;
 }
