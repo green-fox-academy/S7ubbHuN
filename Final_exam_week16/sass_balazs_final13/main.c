54d240854d2d0b378d1ec50651abfabfeed49880
S7ubbHuN <sassbalazs@gmail.com>
2018-02-02 10:25:12 +0100

    adds working nearest_to_average finder method.

--- Final_exam_week16/sass_balazs_final13/main.c
+++ Final_exam_week16/sass_balazs_final13/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "math.h"
 
 /*Create a function that takes an array of integers and returns with the nearest integer in the array compared to the average of the array.*/
 
@@ -9,23 +10,29 @@ int main() {
     unsigned int i = 0;
     unsigned int sum = 0;
     float average = 0;
+    unsigned int nearest = 0;
 
     printf("Enter the first element of the array:");
     scanf("%d", &first_element);
     printf("Enter the last element of the array:");
     scanf("%d", &last_element);
+    unsigned int arrsize = (last_element - first_element) + 1;
+    int array[arrsize];
 
-    int array[last_element - first_element];
-    unsigned j = (last_element - first_element);
-
-    while (i <= j) {
+    while (i < arrsize) {
         array[i] = (first_element + i);
         sum += array[i];
         i++;
     }
-    average = (float)sum / (float)(j + 1);
-    printf("%.2f\n", average);
-
+    average = (float)sum / (float)arrsize;
+    nearest = array[0];
+    for (i = 1; i < arrsize; i++) {
+        if ( fabs (average - array[i]) < fabs (average - nearest)) {
+            nearest = array[i];
+        }
+    }
+    printf("The average of the array is: %.2f\n", average);
+    printf("The nearest number to the average is: %d\n", nearest);
 
 
 
