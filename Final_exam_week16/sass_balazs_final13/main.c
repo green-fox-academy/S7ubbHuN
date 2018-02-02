14ec6b3f9ab61726b6d54244cb7021c793b59e87
S7ubbHuN <sassbalazs@gmail.com>
2018-02-02 09:57:22 +0100

    adds user input to fill the array and count sum, average

--- Final_exam_week16/sass_balazs_final13/main.c
+++ Final_exam_week16/sass_balazs_final13/main.c
@@ -5,9 +5,30 @@
 
 int main() {
 
-    //testpush
+    unsigned int first_element, last_element;
+    unsigned int i = 0;
+    unsigned int sum = 0;
+    float average = 0;
+
+    printf("Enter the first element of the array:");
+    scanf("%d", &first_element);
+    printf("Enter the last element of the array:");
+    scanf("%d", &last_element);
+
+    int array[last_element - first_element];
+    unsigned j = (last_element - first_element);
+
+    while (i <= j) {
+        array[i] = (first_element + i);
+        sum += array[i];
+        i++;
+    }
+    average = (float)sum / (float)(j + 1);
+    printf("%.2f\n", average);
 
 
-    printf("Hello world!\n");
+
+
+
     return 0;
 }
