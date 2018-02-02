29803ff2eea3f52f4185d13f9c20482359bbb003
S7ubbHuN <sassbalazs@gmail.com>
2018-02-02 09:38:23 +0100

    test push

---
+++ Final_exam_week16/sass_balazs_final13/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Create a function that takes an array of integers and returns with the nearest integer in the array compared to the average of the array.*/
+
+int main() {
+
+    //testpush
+
+
+    printf("Hello world!\n");
+    return 0;
+}
